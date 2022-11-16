a5c691f40eaec200dae2dbe41c9610fb99963647
tarikgul <tarik@parity.io>
2022-11-16 11:56:43 -0500

    chore; get boilerplate backend working

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,14 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( _, res ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
