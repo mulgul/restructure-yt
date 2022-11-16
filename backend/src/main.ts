2d90b66275b6db2a906459cd3f16e97d09c22dbe
tarikgul <tarik@parity.io>
2022-11-16 15:41:25 -0500

    chore: add prettier

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,11 @@ const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( _, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get('/', (_, res) => {
+	res.send('Hello world!');
+});
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+	console.log(`server started at http://localhost:${port}`);
+});
