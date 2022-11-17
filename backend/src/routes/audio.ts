8b1ca7d4cc2600a4d35084e20eb8cbb4da9c1201
tarikgul <tarik@parity.io>
2022-11-16 21:11:31 -0500

    fix: /audio/metadata

--- backend/src/routes/audio.ts
+++ backend/src/routes/audio.ts
@@ -1,6 +1,8 @@
 import express, { Request } from 'express';
 import { ParamsDictionary, Query } from 'express-serve-static-core';
 
+import { launchProcess } from '../utils/launchProcess';
+
 type IGetRequestHandler = Request<
 	ParamsDictionary,
 	unknown,
@@ -17,10 +19,19 @@ const router = express.Router();
 router.get('/', async function (req: IGetRequestHandler, res) {
 	const { query } = req;
 	const decodedURI = decodeURIComponent(query.encodedURI);
+	const cmd = `youtube-dl --dump-json ${decodedURI}`;
+	const jsonDump = await launchProcess(cmd);
 
-	res.send({
-		URI: decodedURI,
-	});
+	const json: Record<string, unknown> = JSON.parse(jsonDump);
+	const parsedMetadata = {
+		title: json.title,
+		duration: json.duration,
+		updloadDate: json.upload_date,
+		views: json.view_count,
+		thumbnail: json.thumbnail,
+	};
+
+	res.send(parsedMetadata);
 });
 
 export default router;
