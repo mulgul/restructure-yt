e7b39ae3e01ed01b41d60d430ad38b8cbd3c690c
Ryan Mullen <ryanfmullen@gmail.com>
2022-11-18 13:00:06 -0800

    chore: add lint and yarn berry

--- frontend/src/components/URLInput/index.tsx
+++ frontend/src/components/URLInput/index.tsx
@@ -4,44 +4,33 @@ import { request } from '../../utils/fetch'
 
 const URLInput = () => {
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault()
-    // }
     
     const [url, setUrl]: [string, Function] = useState('');
+    
+    const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+    }
 
 
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log("Hi")
-        console.log(event.target.value);
-    };
-
-    const update = (field: string) => {
-        if(field){
-            console.log("hi")
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value)
+        // console.log(e.target.value.match(/^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/))
+        setUrl(e.target.value)
+        const URLFormInput = document.getElementById("URLForm");
+        if (e.target.value.match(/^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/)){
+            URLFormInput?.classList.remove('BadLink')
+            URLFormInput?.classList.add('GoodLink')
+            request(`/audio/metadata?encodeURI=${encodeURIComponent(e.target.value)}`)
+        } else {
+            URLFormInput?.classList.add('BadLink')
         }
     }
-    // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     console.log(e.target.value)
-    //     console.log(e.target.value.match(/^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/))
-    //     setUrl(e.target.value)
-    //     if (e.target.value.match(/^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/)){
-    //         document.getElementById("URLForm").classList.remove('BadLink')
-    //         document.getElementById("URLForm").classList.add('GoodLink')
-    //         request(`/audio/metadata?encodeURI=${encodeURIComponent(e.target.value)}`)
-    //     } else {
-    //         document.getElementById("URLForm").classList.add('BadLink')
-    //     }
-    // }
-
-
 
     return(
         <div>
-            <form onChange={()=> update("hi")}>
-                {/* <input id='URLForm' type="url" value={url} placeholder="Enter a URL" onChange={(e : React.ChangeEvent<HTMLInputElement>) => handleChange(e)}/> */}
-                <input value={url} onKeyDown={()=>update("hi")}/>
+            <form onSubmit={(e : React.ChangeEvent<HTMLFormElement>) => handleSubmit(e)}>
+                <input id='URLForm' type="url" value={url} placeholder="Enter a URL" onChange={(e : React.ChangeEvent<HTMLInputElement>) => handleChange(e)}/>
+                {/* <input value={url} onKeyDown={()=>update("hi")}/> */}
             </form>
         </div>
     )
