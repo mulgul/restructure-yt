e16acb4a960483b9c9c56b858f5f824ec69a9b4b
tarikgul <tarik@parity.io>
2022-11-19 16:26:33 -0500

    add jest and tests for errorMiddleware

---
+++ backend/src/middlewares/error/errorMiddleware.spec.ts
@@ -0,0 +1,53 @@
+import { BadRequest, InternalServerError } from 'http-errors';
+
+import { errorMiddleware } from './errorMiddleware';
+import {
+	callsNextWithErr,
+	callsNextWithSentHeaders,
+	catchesErrWithStatus,
+} from './testTools';
+
+const errorMiddlewareCallsNextWithErr = callsNextWithErr(errorMiddleware);
+
+const errorMiddlewareCatchesErrWithStatus =
+	catchesErrWithStatus(errorMiddleware);
+
+describe('errorMiddleware', () => {
+	// Necessary since the consolveOverride is called after the getter for the logger is launced
+	beforeAll(() => {
+		jest.spyOn(console, 'log').mockImplementation(() => ({}));
+	});
+
+	errorMiddlewareCallsNextWithErr('ILegacyError', {
+		error: 'legacy error',
+		statusCode: 500,
+	});
+
+	errorMiddlewareCallsNextWithErr('IBasicError', {
+		error: 'basic error',
+	});
+
+	errorMiddlewareCallsNextWithErr('nonsense object', {
+		veryImportantMessage: 'NOT',
+	});
+
+	errorMiddlewareCatchesErrWithStatus(
+		'Error',
+		new Error('This is an error'),
+		500
+	);
+
+	errorMiddlewareCatchesErrWithStatus(
+		'BadRequest (http-error which extends Error) (code gets changed to 500)',
+		new BadRequest('bad request'),
+		500
+	);
+
+	errorMiddlewareCatchesErrWithStatus(
+		'InternalServerError (http-error which extends Error)',
+		new InternalServerError('internal error'),
+		500
+	);
+
+	callsNextWithSentHeaders(errorMiddleware, new Error('This is an error'));
+});
