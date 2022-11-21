6423b208fc2152398f7666e347a08f781fa8aaeb
Ryan Mullen <ryanfmullen@gmail.com>
2022-11-20 18:10:52 -0800

    fix: videoData component populating

--- frontend/src/components/VideoData/index.tsx
+++ frontend/src/components/VideoData/index.tsx
@@ -1,27 +1,25 @@
 import './VideoData.css'
-import { MetadataProps } from '../../types/responses'
+import { IParsedMetadata, MetadataProps } from '../../types/responses'
 
 
 const VideoData = ({metadata}: MetadataProps) => {
 
+    if(Object.keys(metadata).length === 0) return <div></div>;
 
     return ( 
         <div className="VideoData-container">
             <div className='Video-thumbnail'>
-                <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=0.672xw:1.00xh;0.166xw,0&resize=640:*" alt="" />
-                {/* <img src={metadata.thumbnail} alt="" /> */}
-                {/* <p>{metadata.duration}</p> */}
-                <p>10:21</p>
+                <img src={(metadata as IParsedMetadata).thumbnail} alt="" />
+                <div className="Video-duration">
+                    <p>{(metadata as IParsedMetadata).duration}</p>
+                </div>
             </div>
             <div className='Video-info'>
-                {/* <p className="Video-title">{metadata.title}</p> */}
-                <h2 className="Video-title">Title</h2>
+                <h2 className="Video-title">{(metadata as IParsedMetadata).title}</h2>
                 <p>Channel</p>
                 <div className='Video-views-likes'>
-                    <p>53 views</p>
-                    {/* <p>{metadata.views} views</p> */}
-                    <p>10 years ago</p>
-                    {/* <p>{metadata.uploadDate}</p> */}
+                    <p>{(metadata as IParsedMetadata).views} views</p>
+                    <p>{(metadata as IParsedMetadata).uploadDate}</p>
                 </div>
             </div>
         </div>
