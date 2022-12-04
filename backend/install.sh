7612aa78d106f341282ce4e17722bea9a047c0ff
tarikgul <tarik@parity.io>
2022-12-03 22:26:09 -0500

    add bash script to download necessary files if they dont exists

---
+++ backend/install.sh
@@ -0,0 +1,34 @@
+uname_out="$(uname -s)"
+case "${uname_out}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Darwin;;
+esac
+
+echo ${machine}
+
+declare -a pkgs=("ffmpeg" "yt-dlp")
+if [[ $machine = "Darwin" ]]
+then
+    command -v brew >/dev/null 2>&1 || (echo "Homebrew is not installed. Exiting." && exit 1); 
+    for pkg in "${pkgs[@]}"; do
+        install=false
+
+        t=`which $pkg`
+        [ -z "$t" ] && echo "${pkg} isn't installed!" && $install=true
+
+        if $install
+        then
+            brew install $pkg
+        else
+            brew upgrade $pkg
+        fi
+    done
+elif [[ $machine = "Linux" ]]
+then
+    for pkg in "${pkgs[@]}"; do
+        status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
+        if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
+            sudo apt install "$pkg"
+        fi
+    done
+fi
