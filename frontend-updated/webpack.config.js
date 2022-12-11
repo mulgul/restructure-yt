ada67de8f788b15b2e20ff6c666efe4293c279e7
tarikgul <tarik@parity.io>
2022-12-11 18:41:55 -0500

    change folder structure

--- frontend-updated/webpack.config.js
+++ frontend-updated/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './index.tsx',
+    entry: './src/index.tsx',
     devtool: 'inline-source-map',
     output: {
         path: path.join(__dirname, '/dist'),
@@ -27,7 +27,7 @@ module.exports = {
     },
     plugins:[
         new HtmlWebpackPlugin({
-            template: './index.html'
+            template: './public/index.html'
         })
     ]
 }
