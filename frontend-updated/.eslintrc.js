fc180446ac6dd76a9ec5c0b7069228aa80267049
tarikgul <tarik@parity.io>
2022-12-11 19:03:12 -0500

    add prettier configs

--- frontend-updated/.eslintrc.js
+++ frontend-updated/.eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
   extends: [
+    "eslint:recommended",
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
+    "prettier",
   ],
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', "prettier"],
   env: {
     browser: true,
     es6: true,
