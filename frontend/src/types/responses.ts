672316eb1eae76bff41906f43b204ab48a140957
tarikgul <tarik@parity.io>
2022-12-30 17:56:19 -0500

    add response types

---
+++ frontend/src/types/responses.ts
@@ -0,0 +1,39 @@
+export interface IParsedMetadata {
+	title: string;
+	duration: number;
+	uploadDate: string;
+	views: number;
+	thumbnail: string;
+	formats: IFormat[];
+}
+
+export interface IFormat {
+	format_id: string;
+	manifest_url: string;
+	ext: string;
+	width: number | null;
+	height: number | null;
+	tbr: number;
+	asr: number;
+	abr: number;
+	fps: number | null;
+	filesize: number;
+	language: string | null;
+	format_note: string;
+	container: string;
+	vcodec: string;
+	acodec: string;
+	url: string;
+	fragment_based_url: string;
+	fragments: IFragments[];
+	protocol: string;
+	format: string;
+	http_headers: Record<string, string>;
+	downloader_options: { http_chunk_size: number };
+	quality: number;
+}
+
+interface IFragments {
+	path: string;
+	duration: number;
+}
