f89064de336a36887b4eed1f0f4d61dad575c447
Ryan Mullen <ryanfmullen@gmail.com>
2023-01-25 10:14:13 -0800

    updated a bunch of css and website background image

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 
 export const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div>
+			<div>Footer</div>
+			<p>Photo by <a href="https://unsplash.com/es/@simonppt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">SIMON LEE</a> on <a href="https://unsplash.com/photos/zft-W1kVEhg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+		</div>
+	)
 };
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -23,7 +23,11 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ]
     },
     resolve: {
