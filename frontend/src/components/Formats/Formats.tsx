8121bef2daca29c14867c6f08a7172abf4a5ad43
Ryan Mullen <ryanfmullen@gmail.com>
2023-02-10 17:03:32 -0800

    update format card and seach button styling

--- frontend/src/components/Formats/Formats.tsx
+++ frontend/src/components/Formats/Formats.tsx
@@ -45,7 +45,7 @@ export const Formats = ({ meta, url }: IMetadataProp) => {
 	return (
 		<div className="formats-container">
 			<div className="formats-title">
-				<h2>Formats</h2>
+				<h2>Downloadable Formats</h2>
 			</div>
 			<div className="format-cards-parent">
 				{filterFormats(meta.formats).map((format) => (
